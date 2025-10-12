c9b0454baf606acc986ab5c17302c3ff2fd46c4e
lucianobs2 <lucianobs1_outlook.com>
2025-10-11 23:56:43 -0300

    feat: add jwt guards autentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import '@dotenvx/dotenvx/config';
 
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './modules/auth/auth.guard';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
@@ -8,6 +10,11 @@ import { DatabaseModule } from './shared/database/database.module';
 @Module({
   imports: [UsersModule, DatabaseModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { DatabaseModule } from 'src/shared/database/database.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -6,6 +7,6 @@ import { UsersService } from './users.service';
 @Module({
   imports: [DatabaseModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, JwtService],
 })
 export class UsersModule {}
