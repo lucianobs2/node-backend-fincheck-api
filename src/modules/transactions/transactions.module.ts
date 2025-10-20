c817ab6136a15fa7deafe0e1be26984e3950145b
lucianobs2 <lucianobs1_outlook.com>
2025-10-20 08:06:33 -0300

    feat: update transaction

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -1,18 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BankAccountsModule } from '../bank-accounts/bank-accounts.module';
-import { ValidateBankAccountOwnershipService } from '../bank-accounts/services/validate-bank-account-ownership.service';
 import { CategoriesModule } from '../categories/categories.module';
-import { ValidateCategoryOwnershipService } from '../categories/services/validate-category-ownership.service';
+import { TransactionsService } from './services/transactions.service';
+import { ValidateTransactionOwnershipService } from './services/validate-transaction-ownership.service';
 import { TransactionsController } from './transactions.controller';
-import { TransactionsService } from './transactions.service';
 
 @Module({
   imports: [BankAccountsModule, CategoriesModule],
   controllers: [TransactionsController],
-  providers: [
-    TransactionsService,
-    ValidateBankAccountOwnershipService,
-    ValidateCategoryOwnershipService,
-  ],
+  providers: [TransactionsService, ValidateTransactionOwnershipService],
 })
 export class TransactionsModule {}
